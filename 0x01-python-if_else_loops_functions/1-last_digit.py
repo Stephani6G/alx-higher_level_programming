ddb0ec8f65ea2ffe9b2214891f83174a6d68918d
Stephani6G <stephaniegloria283@gmail.com>
2024-01-16 11:23:41 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 print(f"Last digit of {number}", end = " ")
 divide = number % 10
 if number < 0 and divide != 0:
-    print(f"is {(divide - 10)}", end " ")
+    print(f"is {(divide - 10)}", end = " ")
 else:
     print(f"is {divide}", end = " ")
 if(number < 0 or divide < 6) and divide != 0:
