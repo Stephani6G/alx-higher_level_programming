735c1bae38e379b01b571933840ca214793e0a3c
Stephani6G <stephaniegloria283@gmail.com>
2024-01-17 16:57:26 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    length = len(argv) - 1
+
+    print("{0} argument".format(length), end="")
+
+    if length == 0:
+        print("s.")
+    else:
+        if length == 1:
+            print(":")
+        else:
+            print("s:")
+
+        for i in range(length):
+            print("{0}: {1}".format(i + 1, argv[i + 1]))
