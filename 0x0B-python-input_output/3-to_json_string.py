cc1abeb4ab86942333b7ca5a502be2a51e7c6e19
Stephani6G <stephaniegloria283@gmail.com>
2024-02-20 15:27:46 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""defines a string-to-JSON function"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns JSON representation of a string object"""
+    return json.dumps(my_obj)
