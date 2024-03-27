e00e46a48fdddf715fd432520d666055e9656ffb
Stephani6G <stephaniegloria283@gmail.com>
2024-03-27 12:25:22 -0700

    sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates the db hbtn_0d_usa
+-- creates table states in the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
