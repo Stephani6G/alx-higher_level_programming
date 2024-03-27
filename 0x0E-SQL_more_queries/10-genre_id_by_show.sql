684fa9d9ce310f17f7982286cba29b9b4d3a1ba6
Stephani6G <stephaniegloria283@gmail.com>
2024-03-27 12:07:29 -0700

    sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists  shows contained in the hbtn_0d_tvshows db
+-- each record should display tv_shows.title, tv_shows_genres.genre_id
+-- Ordered in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- lists  shows in the db
+-- display tv_shows.title, tv_show_genres.genre_id
+-- order by tv_shows.title and tv_show_genres.genre_id
+-- if show doesn't have a genre display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
